ed9acc493b0d8abfbad4e8313b195a2c705ff94e
Samruddhi <khandekarsamruddhi26@gamil.com>
2024-04-05 21:34:02 +0530

    add.cpp committed

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b){
+	cout<<"/n"<<a+b;
+}
+int main{
+int x,y;
+cout<<"enter value for x:";
+cin>>x;
+cout<<"entr value for y:";
+cin>>y;
+add(x,y);
+}
